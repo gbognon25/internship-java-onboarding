847a6753cc612099d3b0fa860d63b6b90a54a1dc
soung90 <sounglikane@gmail.com>
2025-01-18 22:30:10 +0900

    Initial commit

---
+++ src/main/java/com/sparta/internshipjavaonboarding/InternshipJavaOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.internshipjavaonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipJavaOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipJavaOnboardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/internshipjavaonboarding/InternshipJavaOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.internshipjavaonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipJavaOnboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
